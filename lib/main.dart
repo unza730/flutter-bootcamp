8d0a86d3f6879178f644954a345e27feeb019687
unza730 <unzasohail819@gmail.com>
2022-10-27 07:50:14 +0500

    Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  MyApp({Key? key}) : super(key: key);
+  int x = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,37 +17,49 @@ class MyApp extends StatelessWidget {
             title: Text('I am Rich'),
           ),
           body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Hello from row"),
-                    Icon(Icons.abc_outlined),
-                  ],
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow[900],
-                  child: const Center(child: Text("Hello Unza")),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow[900],
-                  child: const Center(child: Text("Hello Unza")),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow[900],
-                  child: const Center(child: Text("Hello Unza")),
-                ),
-              ],
+            // child: Column(
+            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //   children: [
+            //     Row(
+            //       mainAxisAlignment: MainAxisAlignment.center,
+            //       children: [
+            //         Text("Hello from row"),
+            //         Icon(Icons.abc_outlined),
+            //       ],
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.yellow[900],
+            //       child: const Center(child: Text("Hello Unza")),
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.yellow[900],
+            //       child: const Center(child: Text("Hello Unza")),
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.yellow[900],
+            //       child: const Center(child: Text("Hello Unza")),
+            //     ),
+            //   ],
+            // ),
+            child: Center(
+              child: Text(
+                x.toString(),
+                style: TextStyle(fontSize: 50),
+              ),
             ),
-          )),
+          ),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                x++;
+                print(x.toString());
+              },
+              child: Icon(Icons.add))),
     );
   }
 }
