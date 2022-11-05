42325ee4e0475ccd5c6d1e21cfb35de14e86c0ab
unza730 <unzasohail819@gmail.com>
2022-11-05 12:09:03 +0500

    Drawer Learning

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,6 +17,46 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('NAvigation Drawer'),
         backgroundColor: Color.fromARGB(255, 93, 115, 214),
       ),
+      drawer: Drawer(
+        child: ListView(
+          // listview is just like to Column
+          children: [
+            // DrawerHeader(child: Text('Unza Sohail'))
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 39, 114, 176),
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=600"),
+              ),
+              accountName: Text('Unza Sohail'),
+              accountEmail: Text('unzasohail@gmail.com'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Screen Two'),
+              onTap: () {
+                Navigator.pushNamed(context, ScreenTwo.id);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home Screen'),
+              onTap: () {
+                Navigator.pushNamed(context, HomeScreen.id);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.login_outlined),
+              title: Text('Logout'),
+              onTap: () {
+                Navigator.pushNamed(context, ScreenTwo.id);
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
